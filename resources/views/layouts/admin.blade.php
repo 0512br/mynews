f0504ea048f650c98adbb070781258f7ca81cc16
Ouchi Asaka <asa.pf1205@gmail.com>
2020-10-04 15:49:28 +0000

    PHP/Laravel12の課題

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -37,7 +37,31 @@
                         <!--Left side of Navbar-->
                         <ul class="navbar-nav mr-auto"></ul>
                         <!--Right side of Navbar-->
-                        <ul class="navbar-nav ml-auto"></ul>
+                        <ul class="navbar-nav ml-auto">
+                            {{-- PHP/Laravel 12で追記 --}}
+                            <!--Authentication Links-->
+                            {{-- ログインしていなかった場合、ログイン画面を表示 --}}
+                            @guest
+                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                            {{-- ログインしていた場合、ユーザー名とログアウトボタンを表示 --}}
+                            @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" date-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                    <span class="caret"></span>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}" outlink="event.preventDefauly(); document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                            @endguest
+                            {{-- 12追記おわり --}}
+                        </ul>
                     </div>
                 </div>
             </nav>
--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,16 @@ Route::get('/', function () {
 });
 
 //09で追記
+//12ログイン画面にリダイレクトする設定
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create','Admin\NewsController@add');
+    Route::get('news/create','Admin\NewsController@add')->middleware('auth');
 });
 
 //課題09
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('profile/create','Admin\ProfileController@add');
-    Route::get('profile/edit','Admin\ProfileController@edit');
+    Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
