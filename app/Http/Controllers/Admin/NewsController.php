c840e395d1d6313370db33e95d32441b66d3d4ba
Ouchi Asaka <asa.pf1205@gmail.com>
2020-11-21 00:35:02 +0000

    どこ変更したか忘れたのでいったんコミット

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -7,8 +7,8 @@ use App\Http\Controllers\Controller;
 
 use App\News;
 use App\History;
-
 use Carbon\Carbon;
+use Storage; //画像のアップロードのため追記
 
 class NewsController extends Controller
 {
@@ -29,8 +29,8 @@ class NewsController extends Controller
         $form = $request->all();
         //画像が送信されたら保存して$news->image_pathに画像のパスを保存する
         if (isset($form['image'])) {
-            $path = $request->file('image')->store('public/image');
-            $news->image_path = basename($path);
+            $path = Storage::disk('s3')->putFile('/',$form['image'], 'public'); //二行画像のアップロードのため追記
+            $news->image_path = Storage::disk('s3')->url($path);
         } else {
             $news->image_path = null;
         }
@@ -72,9 +72,10 @@ class NewsController extends Controller
         
         if ($request->remove == 'true') {
             $news_form['image_path'] = null;
-        } elseif ($request->file('image')) {
-            $path = $request->file('image')->store('public/image');
-            $news_form['image_path'] = basename($path);
+        } elseif (isset($news_form["image"])) {
+            // $path = $request->file('image')->store('public/image');
+            $path = Storage::disk('s3')->putFile('/', $news_form['image'], 'public');
+            $news_form['image_path'] = Storage::disk('s3')->url($path);
         } else {
             $news_form['image_path'] = $news->image_path;
         }
